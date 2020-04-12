7d5137830e6e8897d5b0f021da3e31de3c3efc45
Adiomojeed <adio.mojeed@gmail.com>
2020-04-12 23:50:56 +0100

    Styled the calculator

--- src/components/ButtonPanel.jsx
+++ src/components/ButtonPanel.jsx
@@ -18,37 +18,66 @@ class ButtonPanel extends Component {
 		return (
 			<div className="container">
 				<div className="btn-row">
-					<Button name="%" clickHandler={this.handleClick} />
-					<Button name="+/-" clickHandler={this.handleClick} />
+					<Button
+						name="%"
+						clickHandler={this.handleClick}
+						color="text-grey"
+					/>
+					<Button
+						name="+/-"
+						clickHandler={this.handleClick}
+						color="text-grey"
+					/>
 					<Button
 						name="AC"
 						clickHandler={this.handleClick}
-						color="bg-red"
+						color="text-red"
+					/>
+					<Button
+						name="÷"
+						clickHandler={this.handleClick}
+						color="text-grey"
 					/>
 				</div>
 				<div className="btn-row">
 					<Button name="7" clickHandler={this.handleClick} />
 					<Button name="8" clickHandler={this.handleClick} />
 					<Button name="9" clickHandler={this.handleClick} />
-					<Button name="÷" clickHandler={this.handleClick} />
+					<Button
+						name="x"
+						clickHandler={this.handleClick}
+						color="text-grey"
+					/>
 				</div>
 				<div className="btn-row">
 					<Button name="4" clickHandler={this.handleClick} />
 					<Button name="5" clickHandler={this.handleClick} />
 					<Button name="6" clickHandler={this.handleClick} />
-					<Button name="x" clickHandler={this.handleClick} />
+					<Button
+						name="-"
+						clickHandler={this.handleClick}
+						color="text-grey"
+					/>
 				</div>
 				<div className="btn-row">
 					<Button name="1" clickHandler={this.handleClick} />
 					<Button name="2" clickHandler={this.handleClick} />
 					<Button name="3" clickHandler={this.handleClick} />
-					<Button name="-" clickHandler={this.handleClick} />
+					<Button
+						name="+"
+						clickHandler={this.handleClick}
+						color="text-grey"
+					/>
 				</div>
 				<div className="btn-row">
 					<Button name="." clickHandler={this.handleClick} />
 					<Button name="0" clickHandler={this.handleClick} />
-					<Button name="=" clickHandler={this.handleClick} />
-					<Button name="+" clickHandler={this.handleClick} />
+					<Button name="00" clickHandler={this.handleClick} />
+					<Button
+						name="="
+						clickHandler={this.handleClick}
+						color="text-grey"
+					/>
 				</div>
 			</div>
 		);
